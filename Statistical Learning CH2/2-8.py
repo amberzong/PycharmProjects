d2abeea5f3df7f603d00b9a800d5948c00330611
amberzong <amber_zong@126.com>
2016-08-23 19:35:23 +0200

    2-8

--- Statistical Learning CH2/2-8.py
+++ Statistical Learning CH2/2-8.py
@@ -5,20 +5,38 @@ import scipy
 
 
 location='/Users/amberzong/Study/Python + Statistics/dataset/College.csv'
-df = pd.read_csv(location, index_col='Unnamed: 0')
+df = pd.read_csv(location)
+df = df.rename(columns={'Unnamed: 0': 'University'})
 print df.head()
+print df.mean()
 
 #Making pivot table
 table=pd.pivot_table(df,values=['Apps','Accept','Enroll','Top10perc','Top25perc','F.Undergrad','P.Undergrad'],columns='Private',aggfunc=[np.sum,np.mean])
 print table
 
 #making scatter matrix
-pd.tools.plotting.scatter_matrix(df, alpha=0.2, figsize=(15,15), diagonal='kde')
+#pd.tools.plotting.scatter_matrix(df, alpha=0.2, figsize=(15,15), diagonal='kde')
 
-#box plot
+#box plot data
 data = df[['Private','Outstate']]
-data.boxplot(by='Private')
+#add columun Elite and side-by-side box plot outstate vs Elite
+df[['Elite']]=df[['Top10perc']]>50
+data2=df[['Elite','Outstate']]
 
+
+print pd.pivot_table(df,values=['University'],columns='Elite',aggfunc='count')
+
+#box
+data2.boxplot(by='Elite')
+plt.title('Outstate by Elite')
+data.boxplot(by='Private')
+plt.title('Outstate by Private')
+#Histogram
+
+df[['Books']].plot.hist(stacked=True, bins=30)
+plt.title('Book Histogram bin30')
+df[['Outstate']].plot.hist(stacked=True,bins=10)
+plt.title('Outstate Histogram bin10')
 
 print 'Done!'
 plt.show()
